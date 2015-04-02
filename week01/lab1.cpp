a6611aad38d6181db7198a44522771b89fb16523
jkuo005 <jkuo005@ucr.edu>
2015-04-02 16:24:44 -0700

    lab1
    first lab

---
+++ week01/lab1.cpp
@@ -0,0 +1,16 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// @file lab1.cpp
+/// @brief Lab 1
+///
+/// @author Jeff Kuo [jkuo005@ucr.edu]
+/// @date March 31, 2015
+//  ================== END ASSESSMENT HEADER ===============
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, Welcome to CS 10!" << endl;
+    return 0;
+}
