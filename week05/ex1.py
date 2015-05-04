b7d8bdec35c6e1ba8ba411dd666b46c6cb901bd4
jkuo005 <jkuo005@ucr.edu>
2015-05-03 20:42:09 -0700

    Lab5 ex1
    Quick update

--- week05/ex1.py
+++ week05/ex1.py
@@ -19,7 +19,7 @@ def P(x):
     return 924.*(x**6) - 2772.*(x**5) + 3150.*(x**4) - 1680.*(x**3) + 420.*(x**2) - 42*x + 1
     
 #####################
-# Plotting
+# Plotting (ex1a)
 #####################
 x = np.linspace(0,1,num=200,dtype=float)
 y = P(x)
@@ -34,22 +34,22 @@ plt.plot(x,t,'black',linewidth=1)
 #save file
 plt.savefig('Polynomial Plot.png', format = 'png')
 
+#####################
+# Finding Roots (ex1b)
+#####################
 """
 Side Note
 Roots roughly at: 
-x0 = 0.034
-x1 = 0.169
-x2 = 0.381
-x3 = 0.618
-x4 = 0.830
-x5 = 0.966
+x1 = 0.034
+x2 = 0.169
+x3 = 0.381
+x4 = 0.618
+x5 = 0.830
+x6 = 0.966
 """
 roots = np.array((0.034,0.169,0.381,0.618,0.830,0.966))
 solve = np.zeros(roots.size)
 
-#####################
-# Finding Roots
-#####################
 for i in range(roots.size):
     solve[i] = opt.newton(P, roots[i], tol=1.e-10, maxiter=50)
     print "x%s =" %(i+1), solve[i]
